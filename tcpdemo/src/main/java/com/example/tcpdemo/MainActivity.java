7a422b5817f1eee9b7792aca287b8e5e72c00179
xcb <xiecb@gzyitop.com>
2018-02-08 16:01:46 +0800

    socket 通讯

--- tcpdemo/src/main/java/com/example/tcpdemo/MainActivity.java
+++ tcpdemo/src/main/java/com/example/tcpdemo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.tcpdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuAdapter;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     private Button server, client;
+    private static String[] PERMISSION_STORGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         server = (Button) findViewById(R.id.bt_server);
         client = (Button) findViewById(R.id.bt_client);
-
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, PERMISSION_STORGE, 1000);
+        }
         server.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
+            /*    Intent intent = new Intent();
                 intent.setClass(MainActivity.this,ServerActivity.class);
-                startActivity(intent);
+                startActivity(intent);*/
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        FileParseUtils.parseTxtFile(new File("/sdcard/Android/a.txt"));
+                    }
+                }).start();
+
             }
         });
 
@@ -29,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this,ClientActivity.class);
+                intent.setClass(MainActivity.this, ClientActivity.class);
                 startActivity(intent);
             }
         });
